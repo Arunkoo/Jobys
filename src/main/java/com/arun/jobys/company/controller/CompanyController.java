e9c20d30c15538de922dba079eaa923d3c1feb1b
Arun Kumar <pheron408@gmail.com>
2026-08-20 18:03:34 +0530

    feat(config): added reusable prefix support for all apis

--- src/main/java/com/arun/jobys/company/controller/CompanyController.java
+++ src/main/java/com/arun/jobys/company/controller/CompanyController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/companies")
+@RequestMapping("/companies")
 public class CompanyController {
 
     @GetMapping(version = "1.0")
--- src/main/java/com/arun/jobys/config/web/WebConfig.java
+++ src/main/java/com/arun/jobys/config/web/WebConfig.java
@@ -3,6 +3,7 @@ package com.arun.jobys.config.web;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.MediaType;
 import org.springframework.web.servlet.config.annotation.ApiVersionConfigurer;
+import org.springframework.web.servlet.config.annotation.PathMatchConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
@@ -11,5 +12,10 @@ public class WebConfig implements WebMvcConfigurer {
     public void configureApiVersioning(ApiVersionConfigurer configurer) {
         configurer.useMediaTypeParameter(MediaType.parseMediaType("application/vnd.jobys+json"), "v")
                 .addSupportedVersions("1.0", "2.0", "3.0").setDefaultVersion("1.0");
+    }
+
+    @Override
+    public void configurePathMatch(PathMatchConfigurer configurer) {
+        configurer.addPathPrefix("/api", controller->true);
     }
 }
