abf1b7f12f647ed7a0c904b7c9249459b4de226e
Arun Kumar <pheron408@gmail.com>
2026-08-20 16:57:43 +0530

    Intialized jobys backend

---
+++ src/main/java/com/arun/jobys/JobysApplication.java
@@ -0,0 +1,13 @@
+package com.arun.jobys;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobysApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobysApplication.class, args);
+	}
+
+}
