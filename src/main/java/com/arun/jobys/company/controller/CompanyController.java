ff80b26c2c51feaa14a0158931b020d621fddbef
Arun Kumar <pheron408@gmail.com>
2026-09-07 17:21:15 +0530

    feat(controller): intialized controller to use companies service  and updated method

--- src/main/java/com/arun/jobys/company/controller/CompanyController.java
+++ src/main/java/com/arun/jobys/company/controller/CompanyController.java
@@ -1,16 +1,28 @@
 package com.arun.jobys.company.controller;
 
+import com.arun.jobys.entity.Company;
+import com.arun.jobys.service.ICompanyService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/companies")
 public class CompanyController {
+    private final ICompanyService companyService;
+
+    @Autowired
+    public CompanyController(ICompanyService companyService){
+        this.companyService = companyService;
+    }
 
     @GetMapping(version = "1.0")
-    public ResponseEntity<String> getAllCompanies(){
-        return ResponseEntity.ok().body("List of companies");
+    public ResponseEntity<List<Company>> getAllCompanies(){
+        List<Company> companyList = companyService.getAllCompanies();
+        return ResponseEntity.ok().body(companyList);
     }
 }
