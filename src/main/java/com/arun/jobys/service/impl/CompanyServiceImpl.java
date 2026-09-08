fad3b80d4f7342c8b413a90106e85e484a749d99
Arun Kumar <pheron408@gmail.com>
2026-09-08 15:55:51 +0530

    feat(service): injected repsitory to perform operation in service layer

--- src/main/java/com/arun/jobys/service/impl/CompanyServiceImpl.java
+++ src/main/java/com/arun/jobys/service/impl/CompanyServiceImpl.java
@@ -1,14 +1,21 @@
 package com.arun.jobys.service.impl;
 
 import com.arun.jobys.entity.Company;
+import com.arun.jobys.repository.CompanyRepository;
 import com.arun.jobys.service.ICompanyService;
 import org.springframework.stereotype.Service;
 import java.util.List;
 
 @Service
 public class CompanyServiceImpl implements ICompanyService {
+    private final CompanyRepository companyRepository;
+
+    public CompanyServiceImpl (CompanyRepository companyRepository) {
+        this.companyRepository = companyRepository;
+    }
+
     @Override
     public List<Company> getAllCompanies() {
-        return List.of();
+        return companyRepository.findAll();
     }
 }
