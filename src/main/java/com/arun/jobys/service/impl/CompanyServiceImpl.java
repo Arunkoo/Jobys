d02c9e348caa89dc2cd5efe4610b12f208e3da10
Arun Kumar <pheron408@gmail.com>
2026-09-09 21:56:10 +0530

    import added

--- src/main/java/com/arun/jobys/service/impl/CompanyServiceImpl.java
+++ src/main/java/com/arun/jobys/service/impl/CompanyServiceImpl.java
@@ -7,6 +7,7 @@ import com.arun.jobys.service.ICompanyService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
