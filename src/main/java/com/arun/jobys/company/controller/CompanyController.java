7dcd726095ae76380e02d22f197c02d3dfcc90a3
Arun Kumar <pheron408@gmail.com>
2026-09-09 20:02:34 +0530

    refractor(controller): updated controller to use lombok annotation

--- src/main/java/com/arun/jobys/company/controller/CompanyController.java
+++ src/main/java/com/arun/jobys/company/controller/CompanyController.java
@@ -2,6 +2,7 @@ package com.arun.jobys.company.controller;
 
 import com.arun.jobys.entity.Company;
 import com.arun.jobys.service.ICompanyService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,13 +13,9 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/companies")
+@RequiredArgsConstructor
 public class CompanyController {
     private final ICompanyService companyService;
-
-    @Autowired
-    public CompanyController(ICompanyService companyService){
-        this.companyService = companyService;
-    }
 
     @GetMapping(version = "1.0")
     public ResponseEntity<List<Company>> getAllCompanies(){
