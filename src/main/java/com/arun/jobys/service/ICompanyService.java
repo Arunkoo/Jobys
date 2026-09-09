f1d67c6245cbb0b801d3c8dbe82882e2bafdffbe
Arun Kumar <pheron408@gmail.com>
2026-09-09 21:50:18 +0530

    refractor(service): changed abstracted method signature to CompanyDto

--- src/main/java/com/arun/jobys/service/ICompanyService.java
+++ src/main/java/com/arun/jobys/service/ICompanyService.java
@@ -1,8 +1,8 @@
 package com.arun.jobys.service;
 
-import com.arun.jobys.entity.Company;
+import com.arun.jobys.dto.CompanyDto;
 import java.util.List;
 
 public interface ICompanyService {
-    List<Company> getAllCompanies();
+    List<CompanyDto> getAllCompanies();
 }
