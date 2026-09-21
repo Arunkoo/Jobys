09ef73edebdd589984eac76460a117f92d733616
Arun Kumar <pheron408@gmail.com>
2026-09-21 15:04:10 +0530

    refractor(company service): moved the service inside company domain

---
+++ server/src/main/java/com/arun/jobys/company/service/ICompanyService.java
@@ -0,0 +1,9 @@
+package com.arun.jobys.company.service;
+
+import com.arun.jobys.dto.CompanyDto;
+
+import java.util.List;
+
+public interface ICompanyService {
+    List<CompanyDto> getAllCompanies();
+}
---
+++ server/src/main/java/com/arun/jobys/company/service/impl/CompanyServiceImpl.java
@@ -0,0 +1,40 @@
+package com.arun.jobys.company.service.impl;
+
+import com.arun.jobys.company.service.ICompanyService;
+import com.arun.jobys.dto.CompanyDto;
+import com.arun.jobys.entity.Company;
+import com.arun.jobys.repository.CompanyRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+@RequiredArgsConstructor
+public class CompanyServiceImpl implements ICompanyService {
+    private final CompanyRepository companyRepository;
+
+    @Override
+    public List<CompanyDto> getAllCompanies() {
+        List<Company> companylist = companyRepository.findAll();
+        return companylist.stream().map(this::transferCompanyDto).collect(Collectors.toList());
+    }
+
+    private CompanyDto transferCompanyDto(Company company){
+        return new CompanyDto(
+                company.getId(),
+                company.getName(),
+                company.getLogo(),
+                company.getIndustry(),
+                company.getSize(),
+                company.getRating(),
+                company.getLocations(),
+                company.getFounded(),
+                company.getDescription(),
+                company.getEmployees(),
+                company.getWebsite(),
+                company.getCreatedAt()
+        );
+    }
+}
