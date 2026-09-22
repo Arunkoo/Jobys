4e9375dcda23a3a3152803456935eb829bba80ce
Arun Kumar <pheron408@gmail.com>
2026-09-22 16:10:46 +0530

    feat(exceptionHandling): added a default global exception to handle unkown exception

--- server/src/main/java/com/arun/jobys/exception/GlobalExceptionHandler.java
+++ server/src/main/java/com/arun/jobys/exception/GlobalExceptionHandler.java
@@ -1,8 +1,24 @@
 package com.arun.jobys.exception;
 
+import com.arun.jobys.dto.ErrorResponseDto;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
+
+import java.time.LocalDateTime;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
-
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorResponseDto> handleException(Exception exception, WebRequest webRequest){
+        ErrorResponseDto errorResponseDto = new ErrorResponseDto(
+                webRequest.getDescription(false),
+                HttpStatus.INTERNAL_SERVER_ERROR,
+                exception.getMessage(),
+                LocalDateTime.now()
+        );
+        return new ResponseEntity<>(errorResponseDto, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
 }
