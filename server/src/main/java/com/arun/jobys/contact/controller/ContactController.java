1a5c85ca1eb58756c6d95cfbc13c5d8a003df3e5
Arun Kumar <pheron408@gmail.com>
2026-09-22 16:37:59 +0530

    refractor: validate each request using validation written in dto record

--- server/src/main/java/com/arun/jobys/contact/controller/ContactController.java
+++ server/src/main/java/com/arun/jobys/contact/controller/ContactController.java
@@ -2,6 +2,7 @@ package com.arun.jobys.contact.controller;
 
 import com.arun.jobys.contact.service.impl.ContactServiceImpl;
 import com.arun.jobys.dto.ContactRequestDto;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -17,7 +18,7 @@ public class ContactController {
     private final ContactServiceImpl contactService;
 
     @PostMapping(version = "1.0")
-    public ResponseEntity<String> saveContactMsg(@RequestBody ContactRequestDto contactRequestDto){
+    public ResponseEntity<String> saveContactMsg(@RequestBody @Valid ContactRequestDto contactRequestDto){
         boolean isSaved = contactService.saveContact(contactRequestDto);
         if(isSaved){
             return ResponseEntity.status(HttpStatus.CREATED)
