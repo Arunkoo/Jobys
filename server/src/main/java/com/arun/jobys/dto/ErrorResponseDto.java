1151f94c468fd1bbb99237f26225563b378bc0b7
Arun Kumar <pheron408@gmail.com>
2026-09-22 15:41:05 +0530

    feat(exceptionHandling): created a response type dto for exceptionHandling

---
+++ server/src/main/java/com/arun/jobys/dto/ErrorResponseDto.java
@@ -0,0 +1,13 @@
+package com.arun.jobys.dto;
+
+import org.springframework.http.HttpStatus;
+
+import java.time.LocalDateTime;
+
+public record ErrorResponseDto(
+        String apiPath,
+        HttpStatus errorCode,
+        String errorMessage,
+        LocalDateTime errorTime
+) {
+}
