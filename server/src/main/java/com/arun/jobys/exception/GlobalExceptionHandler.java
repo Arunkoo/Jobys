794aa9e8e703313ac54cd0f880ad5949121d071a
Arun Kumar <pheron408@gmail.com>
2026-09-23 15:41:49 +0530

    feat(exceptionHandling): handled the exception related Method argument not valid

--- server/src/main/java/com/arun/jobys/exception/GlobalExceptionHandler.java
+++ server/src/main/java/com/arun/jobys/exception/GlobalExceptionHandler.java
@@ -3,11 +3,14 @@ package com.arun.jobys.exception;
 import com.arun.jobys.dto.ErrorResponseDto;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
 
 import java.time.LocalDateTime;
+import java.util.*;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
@@ -21,4 +24,16 @@ public class GlobalExceptionHandler {
         );
         return new ResponseEntity<>(errorResponseDto, HttpStatus.INTERNAL_SERVER_ERROR);
     }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String, String>> handleException(MethodArgumentNotValidException exception){
+        Map<String, String> errors = new HashMap<>();
+        // Take the validation exception, get its validation result, get all the individual field errors
+        // and store those errors in a list.
+        List<FieldError> fieldsErrorList = exception.getBindingResult().getFieldErrors();
+
+        fieldsErrorList.forEach(error->errors.put(error.getField(), error.getDefaultMessage()));
+        return ResponseEntity.badRequest().body(errors);
+    }
+
 }
