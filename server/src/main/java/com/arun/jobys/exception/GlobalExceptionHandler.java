31dec6971a3c699beff221043525257f76f71c1d
Arun Kumar <pheron408@gmail.com>
2026-09-23 16:49:48 +0530

    feat(exceptionHandling): handled a method validation exeption in a fetchopencontact api

--- server/src/main/java/com/arun/jobys/exception/GlobalExceptionHandler.java
+++ server/src/main/java/com/arun/jobys/exception/GlobalExceptionHandler.java
@@ -1,16 +1,20 @@
 package com.arun.jobys.exception;
 
 import com.arun.jobys.dto.ErrorResponseDto;
+import org.springframework.context.MessageSourceResolvable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
+import org.springframework.validation.method.ParameterValidationResult;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.method.annotation.HandlerMethodValidationException;
 
 import java.time.LocalDateTime;
 import java.util.*;
+import java.util.stream.Collectors;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
@@ -35,5 +39,26 @@ public class GlobalExceptionHandler {
         fieldsErrorList.forEach(error->errors.put(error.getField(), error.getDefaultMessage()));
         return ResponseEntity.badRequest().body(errors);
     }
+
+    @ExceptionHandler(HandlerMethodValidationException.class)
+    public ResponseEntity<Map<String, String>> handleException(HandlerMethodValidationException exception){
+        Map<String, String> errors = new HashMap<>();
+
+        List<ParameterValidationResult> results = exception.getParameterValidationResults();
+
+        results.forEach(result-> {
+            String paramName = result.getMethodParameter().getParameterName();
+
+            String combinedMessage = result.getResolvableErrors()
+                    .stream()
+                    .map(MessageSourceResolvable::getDefaultMessage)
+                    .collect(Collectors.joining(","));
+
+            errors.put(paramName, combinedMessage);
+        });
+
+        return ResponseEntity.badRequest().body(errors);
+    }
+
 
 }
